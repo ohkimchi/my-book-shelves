128012dea98c88efd55a29420c140644c81f2d5d
Max Xu <max.xu@biqmind.com>
2019-03-23 13:18:38 +0800

    Updates

---
+++ precache-manifest.bf2f1f75930ded901ada20491acd9e24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0179268e3f6945534a6a",
+    "url": "/my-book-shelves/static/css/main.db0d9249.chunk.css"
+  },
+  {
+    "revision": "0179268e3f6945534a6a",
+    "url": "/my-book-shelves/static/js/main.0f39782e.chunk.js"
+  },
+  {
+    "revision": "3348ce91ee44d4c64e13",
+    "url": "/my-book-shelves/static/js/runtime~main.d835bae8.js"
+  },
+  {
+    "revision": "e96cf970072d1bad8431",
+    "url": "/my-book-shelves/static/js/2.1eb644a3.chunk.js"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-book-shelves/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-book-shelves/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "876d6c05e9575e6bebaebd04bafab868",
+    "url": "/my-book-shelves/static/media/search.876d6c05.png"
+  },
+  {
+    "revision": "f155c8f34f61bc06731278428e508ed1",
+    "url": "/my-book-shelves/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/my-book-shelves/precache-manifest.bf2f1f75930ded901ada20491acd9e24.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/my-book-shelves/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
